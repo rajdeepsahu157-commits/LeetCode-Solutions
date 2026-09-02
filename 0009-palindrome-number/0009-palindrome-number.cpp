faa8f09b685e20df9839f7460b894219a59e7c1c
rajdeepsahu157-commits <rajdeepsahu157@gmail.com>
2026-09-02 16:21:27 +0530

    Time: 3 ms (43.9%), Space: 8.4 MB (92.16%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        bool ans=false ;
+        if(x<0){
+            return ans ;
+        }
+        else{
+            int n=x , lastdig, rev=0;
+            while(n!=0){
+                lastdig=n%10 ;
+                n=n/10 ;
+            if(rev > INT_MAX/10 || rev < INT_MIN/10){
+                return 0 ;
+            }
+                rev=rev*10 + lastdig ;
+            }
+            if(x==rev){
+                ans=true ;
+            } 
+        }
+        return ans ;
+    }
+};
