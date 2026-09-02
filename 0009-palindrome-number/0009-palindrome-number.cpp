2e343911fda6d2a1e53d78d82d6ef1b3901bdf10
rajdeepsahu157-commits <rajdeepsahu157@gmail.com>
2026-09-02 16:25:46 +0530

    Time: 5 ms (18.65%), Space: 8.7 MB (36.1%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -10,7 +10,7 @@ public:
             while(n!=0){
                 lastdig=n%10 ;
                 n=n/10 ;
-            if(rev > INT_MAX/10 || rev < INT_MIN/10){
+                  if(rev > INT_MAX/10 || rev < INT_MIN/10){
                 return 0 ;
             }
                 rev=rev*10 + lastdig ;
