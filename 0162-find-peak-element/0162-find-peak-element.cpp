2a107d49c212cbda608b473b098c8c37545668c5
rajdeepsahu157-commits <rajdeepsahu157@gmail.com>
2026-09-14 15:29:31 +0530

    Time: 0 ms (100%), Space: 12.7 MB (51.91%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,7 +1,35 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-        int max = max_element(nums.begin(), nums.end()) - nums.begin();
-        return max;
+        if(nums.size() == 1){
+            return 0;
+        }
+        else if(nums.size() == 2){
+            if(nums[0]>nums[1]){
+                return 0;
+            }
+            else{
+                return 1;
+            }
+        }
+        else{ int flag=0;
+     for(int i=1;i<nums.size()-1;i++){
+        if(nums[i]>nums[i-1] && nums[i]>nums[i+1]){
+            flag=1;
+            return i;
+            
+        }
+     }
+    if(flag==0){
+        if(nums[0]>nums[nums.size()-1]){
+            return 0;
+        }
+        else{
+            return nums.size()-1;
+        }
+    }
+
+        }
+     return{};
     }
 };
